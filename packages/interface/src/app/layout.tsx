6831cf4c937bc8abf4d58b35f2c08cb357dee4be
teeclaw <teeclaw@proton.me>
2026-03-23 22:24:33 +0700

    add:domain verification

--- packages/interface/src/app/layout.tsx
+++ packages/interface/src/app/layout.tsx
@@ -18,6 +18,8 @@ export const metadata: Metadata = {
   description: "Reputation without opinion.",
   other: {
     "apple-mobile-web-app-title": "zkCred",
+    "talentapp:project_verification":
+      "42a11dde745acaa5e9989d4e6a06660208f5f7103530c1d03b323b2bd5cd4cc9eb17c5d8644260c1327bb7bbff837fb331cfc771b13055fd7a5228042c011bff",
   },
 };
 
